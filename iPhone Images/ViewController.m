fbcf2428eca39529d2d88465f7805de74417ea4f
Thiago Hissa <thiagohissa@gmail.com>
2017-07-17 15:06:27 -0400

    added stretch goal

--- iPhone Images/ViewController.m
+++ iPhone Images/ViewController.m
@@ -19,6 +19,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+//    NSURL *url = [NSURL URLWithString:[self randomImage]];
+//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+//    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//        
+//        
+//        if(error){
+//            NSLog(@"Error...");
+//            return;
+//        }
+//        
+//        NSData *data = [NSData dataWithContentsOfURL:location];
+//        UIImage *image = [UIImage imageWithData:data];
+//        
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            self.myImage.image = image;
+//        }];
+//        
+//    }];
+//    
+//    [downloadTask resume];
+    [self loadData];
+    
+}
+
+
+
+- (IBAction)randomButton:(id)sender {
+    
+    [self loadData];
+    
+}
+
+
+
+-(NSString*)randomImage{
+    int random = arc4random_uniform(3);
+    
+    if(random == 0){
+        self.urlString = @"https://i.imgur.com/CoQ8aNl.png";
+    }
+    else if(random == 1){
+        self.urlString = @"https://i.imgur.com/zdwdenZ.png";
+    }
+    else if(random == 2){
+        self.urlString = @"https://i.imgur.com/2vQtZBb.png";
+    }
+    else if(random == 3){
+        self.urlString = @"https://i.imgur.com/y9MIaCS.png";
+    }
+    NSLog(@"Random: %d", random);
+    
+    return self.urlString;
+}
+
+
+
+
+
+
+-(void)loadData{
+    
+    
     NSURL *url = [NSURL URLWithString:[self randomImage]];
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
@@ -26,7 +89,7 @@
         
         
         if(error){
-            NSLog(@"Erro...");
+            NSLog(@"Error...");
             return;
         }
         
@@ -41,29 +104,6 @@
     
     [downloadTask resume];
     
-    
-}
-
-
-
-- (IBAction)randomButton:(id)sender {
-    [self randomImage];
-}
-
-
-
--(NSString*)randomImage{
-    int random = arc4random_uniform(2);
-    
-    if(random == 1){
-        self.urlString = @"http://imgur.com/CoQ8aNl.png";
-    }
-    else if(random == 0){
-        self.urlString = @"http://imgur.com/zdwdenZ.png";
-    }
-    NSLog(@"Random: %d", random);
-    
-    return self.urlString;
 }
 
 
